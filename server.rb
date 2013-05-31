b1484f2fc64a8ce67ba3a0a8c560f2ad355adb4c
Justin Jaffray <justin.jaffray@gmail.com>
2013-05-31 13:32:29 -0400

    hash alg before saving file

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require "fileutils"
 require "sinatra"
+require 'digest/sha1'
 require_relative "lib/cube_renderer"
 
 get "/" do
@@ -9,6 +10,7 @@ end
 get "/cube/:alg" do
   FileUtils.mkdir("tmp") unless Dir.exist?("tmp")
   fname = params[:alg].gsub("'","PRIME")
+  fname = Digest::SHA1.hexdigest(fname)
   CubeRenderer.new(params[:alg]).draw("tmp/#{fname}.png")
   send_file("tmp/#{fname}.png")
 end
