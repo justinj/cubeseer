283a3642b55112893a07eabaa0561a4700178738
Justin Jaffray <justin.jaffray@gmail.com>
2013-06-02 18:41:31 -0400

    Fixed order of expected and actual

--- test/cube_test.rb
+++ test/cube_test.rb
@@ -10,60 +10,60 @@ end
 class TestNonStandardMoves < MiniTest::Test
   
   def test_F
-    assert_equal Cube.algorithm(3, "F")["LUB:LDF"], [
+    assert_equal [[:L, :L, :D],
                   [:L, :L, :D],
-                  [:L, :L, :D],
-                  [:L, :L, :D]]
+                  [:L, :L, :D]],
+                  Cube.algorithm(3, "F")["LUB:LDF"]
   end
 
   def test_Rw
-    assert_equal Cube.algorithm(3, "Rw")["UBL:UFR"], [
+    assert_equal [[:U, :F, :F],
                   [:U, :F, :F],
-                  [:U, :F, :F],
-                  [:U, :F, :F]]
+                  [:U, :F, :F]],
+                  Cube.algorithm(3, "Rw")["UBL:UFR"]
   end
 
   def test_slices
-    assert_equal Cube.algorithm(3, "M")["UBL:UFR"], [
+    assert_equal [[:U, :B, :U],
                   [:U, :B, :U],
-                  [:U, :B, :U],
-                  [:U, :B, :U]]
-    assert_equal Cube.algorithm(4, "M")["UBL:UFR"], [
+                  [:U, :B, :U]],
+                  Cube.algorithm(3, "M")["UBL:UFR"]
+    assert_equal [[:U, :B, :B, :U],
                   [:U, :B, :B, :U],
                   [:U, :B, :B, :U],
-                  [:U, :B, :B, :U],
-                  [:U, :B, :B, :U]]
-    assert_equal Cube.algorithm(3, "M2")["UBL:UFR"], [
+                  [:U, :B, :B, :U]],
+                  Cube.algorithm(4, "M")["UBL:UFR"]
+    assert_equal [[:U, :D, :U],
                   [:U, :D, :U],
-                  [:U, :D, :U],
-                  [:U, :D, :U]]
-    assert_equal Cube.algorithm(3, "S")["UBL:UFR"], [
-                  [:U, :U, :U],
+                  [:U, :D, :U]],
+                  Cube.algorithm(3, "M2")["UBL:UFR"]
+    assert_equal [[:U, :U, :U],
                   [:L, :L, :L],
-                  [:U, :U, :U]]
-    assert_equal Cube.algorithm(3, "E")["UBL:UFR"], [
+                  [:U, :U, :U]],
+                  Cube.algorithm(3, "S")["UBL:UFR"]
+    assert_equal [[:U, :U, :U],
                   [:U, :U, :U],
-                  [:U, :U, :U],
-                  [:U, :U, :U]]
+                  [:U, :U, :U]],
+                  Cube.algorithm(3, "E")["UBL:UFR"]
   end
   
   def test_rotations
-    assert_equal Cube.algorithm(3, "x")["UBL:UFR"], [
+    assert_equal [[:F, :F, :F],
                   [:F, :F, :F],
-                  [:F, :F, :F],
-                  [:F, :F, :F]]
-    assert_equal Cube.algorithm(3, "y")["UBL:UFR"], [
+                  [:F, :F, :F]],
+                  Cube.algorithm(3, "x")["UBL:UFR"]
+    assert_equal [[:U, :U, :U],
                   [:U, :U, :U],
-                  [:U, :U, :U],
-                  [:U, :U, :U]]
-    assert_equal Cube.algorithm(3, "z")["UBL:UFR"], [
+                  [:U, :U, :U]],
+                  Cube.algorithm(3, "y")["UBL:UFR"]
+    assert_equal [[:L, :L, :L],
                   [:L, :L, :L],
-                  [:L, :L, :L],
-                  [:L, :L, :L]]
+                  [:L, :L, :L]],
+                  Cube.algorithm(3, "z")["UBL:UFR"]
   end
 
   def test_move_with_rotation
-    assert_equal Cube.algorithm(3, "U z")["BUR:BLU"], 
-                 [[:L, :B, :B]]
+    assert_equal [[:L, :B, :B]],
+                  Cube.algorithm(3, "U z")["BUR:BLU"]
   end
 end
